c5dfef18dc83d437383f2dcaf635d7136dc87b2b
Keegan Low <keeganlowce.23@ichat.sp.edu.sg>
2024-04-30 14:59:46 +0800

    Modified

--- bmi.py
+++ bmi.py
@@ -4,12 +4,15 @@ def calculate_bmi (height, weight):
     BMI = weight/(height*height)
     print("BMI is " + str(BMI))
     if BMI < 18.5: 
-        print(-1)
+        print("Under Weight")
+        return -1
     elif 18.5<=BMI<=25:
-        print(0)
+        print("Normal")
+        return 0
     else:
-        print(1)
+        print("Over Weight")
+        return 1
+    
 
 calculate_bmi(weight = 57, height = 1.73)
-
 
